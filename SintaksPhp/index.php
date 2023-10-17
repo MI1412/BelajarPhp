1041cf439d6c0de020dc623aa0c30704f234a929
MI1412 <144302456+MI1412@users.noreply.github.com>
2023-10-17 20:18:23 +0700

    Pertemuan 2 php dasar

---
+++ SintaksPhp/index.php
@@ -0,0 +1,53 @@
+<?php
+// pertemuan 2 - php dasar 
+// sintaks php 
+
+// standar output
+// echo, print
+// print_r
+// var_dump
+
+// penulisan sintaks php
+// 1. php didalam html
+// 2. html didalam php
+
+// variabel dan tipe data
+// variabel 
+// tidak boleh diawali dengan angka tapi boleh mengandung angka
+// $nama = "imron";
+// echo "halo , nama saya $nama " 
+// operator
+// aritmatika 
+// + - * / %
+// $x = 10;
+// $y = 10;
+// echo $x * $y;
+
+// Penggabungan string / concatenation / concat
+// . 
+// $nama_depan = "imron";
+// $nama_belakang = "m";
+// echo $nama_belakang . "" . $nama_depan;
+//  Assigment 
+// //  =, +=, -=, *=, /=, %=, .=
+// $x = 90;
+// $x -= 10;
+// echo $x; 
+// $nama = "imron";
+// $nama .= "";
+// $nama .= "";
+// echo $nama;
+
+//  Perbandingan
+// <, >, <=, >=, ==, !=
+// var_dump(1==1);
+
+// // identitas
+// //  ===, !==, 
+// var_dump(1 === "1");
+
+//  logika
+// &&, ||, !
+$x = 30;
+var_dump($x <20 || $x % 2 == 0)
+?>
