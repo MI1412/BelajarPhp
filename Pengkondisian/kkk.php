b7dc1b8dde327f2465e932f84b37f387963ae6de
MI1412 <imronm1309@gmail.com>
2024-01-18 06:32:32 +0700

    pengkondisian

---
+++ Pengkondisian/kkk.php
@@ -0,0 +1,21 @@
+<?php
+    // pengulangn
+    // for
+    // while
+    // do..whhile
+    // foreach : pengulangan khusus array
+    // for($i = 0; $i < 5; $i++ ){
+    //     echo "hello dunia <br>";
+    // }
+    // $i =0;
+    // while($i < 5 ){
+    //     echo "hello dunia <br> ";
+    //     $i++;
+    // }
+    // $i = 0;
+    // do {
+    //     echo "hello dunia <br>";
+    //     $i++;
+    //    } while( $i < 5 );
+    
+?>
